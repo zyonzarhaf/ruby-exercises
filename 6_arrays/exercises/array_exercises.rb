52d75ffcc2959dd855852e586bbad4f8e73567e6
Zyon Z <zyonzarhaf@gmail.com>
2023-08-23 14:13:41 -0300

    Remove unnecessary explicit return statements

--- 6_arrays/exercises/array_exercises.rb
+++ 6_arrays/exercises/array_exercises.rb
@@ -1,16 +1,16 @@
 def nil_array(number)
   # return an array containing `nil` the given number of times
-  return Array.new(number, nil)
+  Array.new(number, nil)
 end
 
 def first_element(array)
   # return the first element of the array
-  return array.first
+  array.first
 end
 
 def third_element(array)
   # return the third element of the array
-  return array[2]
+  array[2]
 end
 
 def last_three_elements(array)
@@ -25,51 +25,49 @@ end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array.pop
-  return array
+  array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array.shift(3)
-  return array
+  array
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
-  return original + additional
+  original + additional
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
-  return original - comparison
+  original - comparison
 end
 
 def empty_array?(array)
   # return true if the array is empty
-  return array.empty?
+  array.empty?
 end
 
 def reverse(array)
   # return the reverse of the array
-  return array.reverse
+  array.reverse
 end
 
 def array_length(array)
   # return the length of the array
-  return array.length
+  array.length
 end
 
 def include?(array, value)
   # return true if the array includes the value
-  return array.include?(value)
+  array.include?(value)
 end
 
 def join(array, separator)
   # return the result of joining the array with the separator
-  return array.join(separator)
+  array.join(separator)
 end
