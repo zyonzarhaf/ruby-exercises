af4a6eb60d0c2a83b9b6a4ca917a493b0ab283d8
Zyon Z <zyonzarhaf@gmail.com>
2023-09-03 15:58:15 -0300

    Complete nested array exercises

--- 12_nested_collections/exercises/nested_array_exercises.rb
+++ 12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,16 +11,21 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  chart = Array.new(number_of_rows) { Array.new(seats_per_row, nil) }
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,24 +33,33 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+  chart[row_index].delete_at(seat_index)
+  chart
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
-
   # NOTE: `chart` should **not** be mutated
+  result = chart.flatten.reduce(0) do |accumulator, element|
+    if element == nil
+      accumulator += 1
+    end
+    accumulator
+  end
+  result
 end
 
 def find_favorite(array_of_hash_objects)
   # take an array_of_hash_objects and return the hash which has the key/value
   # pair :is_my_favorite? => true. If no hash returns the value true to the key
   # :is_my_favorite? it should return nil
-
   # array_of_hash_objects will look something like this:
   # [
   #   { name: 'Ruby', is_my_favorite?: true },
@@ -55,4 +69,5 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  result = array_of_hash_objects.find { |element| element[:is_my_favorite?] == true}
 end
